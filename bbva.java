a16d17373825e4ca968e9d9cea522ec7a1eaca2f
Noelia <noeliajuarez900@gmail.com>
2024-11-16 10:37:56 -0600

    Hola es el Segundo commit

--- bbva.java
+++ bbva.java
@@ -1,4 +1,22 @@
 package BancoTonyStark;
+class Persona{
+    String nombre;
+    String sexo;
+    int edad;
+
+
+    Persona(String nombre, String sexo, int edad){
+this.nombre=nombre;
+this.edad=edad;
+this.sexo=sexo;
+
+}
+void hablar(){
+    System.out.println("Hola soy una persona");
+    
+}
+}
+
 class Programa{
     public static void main(String[] args){
        System.out.println("Hello world"); 
