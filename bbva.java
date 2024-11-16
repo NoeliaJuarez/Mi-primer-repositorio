64613dab5e5eb35ad61ffd3c4eee9341efe6418d
Noelia <noeliajuarez900@gmail.com>
2024-11-16 12:45:25 -0600

    Hola es el Tercer commit

--- bbva.java
+++ bbva.java
@@ -17,8 +17,49 @@ void hablar(){
 }
 }
 
+class Cliente extends Persona{
+int numero;
+
+Cliente(int numero,String sexo, int edad,String nombre){
+super (nombre,sexo,edad);
+this.numero=numero;
+}
+}
+class Banco{
+    String nombre;
+    Cliente  clientes[];
+
+    Banco(int cantidadClientes,String nombre){
+        clientes = new Cliente[cantidadClientes];
+        this.nombre=nombre;
+    }
+    Cliente crearCliente(String nombre, String sexo, int edad, int numero){
+        System.out.println("Creando cliente.....");
+        Cliente cliente=new Cliente(numero, sexo, edad, nombre);
+
+        for(int i=0; i < clientes.length;i++){
+            if(clientes[i]==null){
+                clientes[i]=cliente;
+                System.out.println("Cliente creado exitosamente");
+                break;
+            }
+        }
+        return cliente;
+    }
+    void listarClientes(){
+        for(int i =0; i < clientes.length;i++){
+            if(clientes[i] !=null){
+                Cliente cliente = clientes[i];
+                System.out.println("Cliente "+ cliente.nombre+"No: " + cliente.numero);
+                
+            }
+        }
+    }
+}
+
 class Programa{
     public static void main(String[] args){
-       System.out.println("Hello world"); 
+       System.out.println("Welcome to the bank"); 
+
     }
 }
